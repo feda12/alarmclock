f5606aa09f541aa8cb40cf83f091d1d6f90d4bfb
Ben LaCam <blacam@musicdealers.com>
2014-03-20 21:06:14 -0500

    Windows built

--- alarmwindow.cpp
+++ alarmwindow.cpp
@@ -1,8 +1,17 @@
 #include "alarmwindow.h"
 
 AlarmWindow::AlarmWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QWidget(parent)
 {
+    _planAlarm = new QPushButton(tr("Plan alarm"), this);
+    _alarmTimeEdit = new QTimeEdit(QTime::currentTime(), this);
+    _alarmTimer = new QTimer(this);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(_alarmTimeEdit);
+    layout->addWidget(_planAlarm);
+    this->setLayout(layout);
+
 }
 
 AlarmWindow::~AlarmWindow()
--- alarmwindow.h
+++ alarmwindow.h
@@ -1,15 +1,25 @@
 #ifndef ALARMWINDOW_H
 #define ALARMWINDOW_H
 
-#include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
+#include <QTimeEdit>
+#include <QTimer>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
-class AlarmWindow : public QMainWindow
+class AlarmWindow : public QWidget
 {
     Q_OBJECT
 
 public:
     AlarmWindow(QWidget *parent = 0);
     ~AlarmWindow();
+
+private:
+    QPushButton *_planAlarm;
+    QTimeEdit *_alarmTimeEdit;
+    QTimer *_alarmTimer;
 };
 
 #endif // ALARMWINDOW_H
